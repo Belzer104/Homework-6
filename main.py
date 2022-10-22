d722bb006b9203ea81acef784915267841b8fe7c
Belzer104 <shablovskyiillia@gmail.com>
2022-10-22 18:49:21 +0300

    Add try/exept

--- main.py
+++ main.py
@@ -95,8 +95,11 @@ def main(folder: Path):
 
 
 if __name__ == '__main__':
-    if sys.argv[1]:
+    try:
         folder_for_scan = Path(sys.argv[1])
+    except IndexError:
+        print('Enter path to folder')
+    else:    
         print(f'Start in folder {folder_for_scan.resolve()}')
         main(folder_for_scan.resolve())
 
